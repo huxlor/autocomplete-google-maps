447b3b47a8affbdda4d71e24104a1e8883f5f0df
Sebastian Diaz <sebastiandiaz@Sebastians-MacBook-Air.local>
2020-09-21 09:42:22 -0500

    Autocomplete Google Maps API - Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
+import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
+import { Address } from 'ngx-google-places-autocomplete/objects/address';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @ViewChild("placesRef") placesRef : GooglePlaceDirective;
+
+  @Input() options: any;
+
   title = 'Google-Address';
+
+  ngOnInit(): void {
+
+  }
+
+  public handleAddressChange(address: Address) {
+    // Do some stuff
+    console.log('Lat', address.geometry.location.lat());
+    console.log('Lng', address.geometry.location.lng());
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    GooglePlaceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
